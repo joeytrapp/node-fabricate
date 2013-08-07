312abd01d4f319e66c26a56af3c029664a07be4f
Joey Trapp <joey@loadsys.com>
2013-08-06 20:31:10 -0500

    Added tests for the default option values

--- test/options_test.js
+++ test/options_test.js
@@ -1,4 +1,5 @@
-var Fabricate = require('../lib/fabricate');
+var Fabricate = require('../lib/fabricate'),
+    path = require('path');
 
 exports.module = {
   testErrorWhenNoOptionsPassed: function(test) {
@@ -26,7 +27,28 @@ exports.module = {
     function newFab() {
       new Fabricate({ src: '' });
     }
-    test.throws(newFab, 'src option is required', 'error thown for missing src option');
+    test.throws(newFab, 'dest option is required', 'error thown for missing dest option');
     test.done();
-  }
+  },
+
+  testSetsUpOptionsOnCreate: function(test) {
+    var fab = new Fabricate({ src: 'test/data/src/app.js', dest: 'test/data/dest/app.js' });
+    test.equal(fab.src, 'app.js', 'src option is set');
+    test.notEqual(fab.include.indexOf('test/data/src'), -1, 'src dirname added to include');
+    test.equal(fab.dest, 'test/data/dest/app.js', 'dest option is set');
+    test.equal(fab.sourceMap, 'app.js.map', 'sourceMap option gets a default value');
+    test.equal(fab.tmpDir, 'tmp', 'tmpDir option gets a default value');
+    test.equal(fab.compress, false, 'compress option gets set to false by default');
+    test.done();
+  },
+
+  testSettingSourceMapToFalseDisablesDefaultValue: function(test) {
+    var fab = new Fabricate({
+      src: 'test/data/src/app.js',
+      dest: 'test/data/dest/app.js',
+      sourceMap: false
+    });
+    test.equal(fab.sourceMap, false, 'sourceMap is not default when passed false');
+    test.done();
+  },
 };
